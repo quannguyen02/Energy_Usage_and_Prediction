3cf6ac6af829d4be37eed83bbadc147ad62b885a
emma lee <emmalee@emmas-MacBook-Pro.local>
2023-01-18 17:14:41 -0500

    slider adding

--- app2.py
+++ app2.py
@@ -1,8 +1,9 @@
 import streamlit as st 
 import pandas as pd, matplotlib as plt
+from PIL import Image 
 
 st.sidebar.markdown("## Select data to visualize")
-st.sidebar
+# st.sidebar
 
 # Add a selectbox to the sidebar:
 side1_selectbox = st.sidebar.selectbox(
@@ -11,12 +12,24 @@ side1_selectbox = st.sidebar.selectbox(
 )
 
 if side1_selectbox == 'Year':
-   side1_slider = st.sidebar.slider(range(2013,2023))
+   side1_slider = st.sidebar.slider(
+      "Range of Years", 2013, 2022)
+   
+   image = Image.open(f'./Graphs/Year/{side1_slider}_Electrical.png')
+   st.image(image, caption= "hello", use_column_width = "always")
+   # uploadfile = st.file_uploader("Choose image", type="png")
+   # if uploadFile is not None:
+   #    img = load_image(uploadFile)
+   #    st.image(img)
+   #    st.write("Image Uploaded Successfully")
+   # else:
+   #    st.write("Make sure you image is in JPG/PNG Format.")
+   
 else: 
    side2_selectbox = st.sidebar.selectbox(
       'Choose Building Type',
       ('Academic Buildings', 'North Residence Halls', 'South Residence Hall')
    )
-   if side2_selectbox("Academic Buildings"):
+   # if side2_selectbox("Academic Buildings"):
       
    
